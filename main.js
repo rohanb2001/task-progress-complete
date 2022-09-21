76ddac234010166bb03fc0ac9ac5b41fcee25d94
Ayan Banerjee <ayanbanerjee102@gmail.com>
2022-09-22 01:14:44 +0530

    Freshly done the forward movement of the lis

--- main.js
+++ main.js
@@ -1,102 +1,151 @@
-const Btn = document.querySelectorAll(".btn");
-const todos = document.querySelector(".todos");
-const li = document.querySelectorAll(".todo");
+import { v4 as uuidv4 } from "https://jspm.dev/uuid";
+
+const taskUl = document.querySelector(".todos");
 const progressUl = document.querySelector(".progress-todos");
 const completeUl = document.querySelector(".complete-todos");
 
 let state = {
+  task: [
+    {
+      name: "AAA",
+      id: uuidv4(),
+      room: 1,
+      moveForward: true,
+      moveBackward: false,
+    },
+    {
+      name: "BBB",
+      id: uuidv4(),
+      room: 1,
+      moveForward: true,
+      moveBackward: false,
+    },
+    {
+      name: "CCC",
+      id: uuidv4(),
+      room: 1,
+      moveForward: true,
+      moveBackward: false,
+    },
+  ],
+
   progress: [],
   complete: [],
 };
 
-// Functions
-function showProgressUI() {
-  if (state.progress.length) {
-    let str = state.progress.reduce((acc, curr) => {
-      return (
-        acc +
-        `
-        <li class="todo">
-            <button class="btn previous">
-              <i class="fa-solid fa-angle-left"></i>
-            </button>
-            ${curr}
+let roomNames = {
+  1: "task",
+  2: "progress",
+  3: "complete",
+};
+
+function renderTask() {
+  let lis = state.task.reduce((acc, curr) => {
+    return (
+      acc +
+      `
+    <li class="todo" data-id=${curr.id}>
+            ${curr.name}
             <button class="btn next">
               <i class="fa-solid fa-angle-right"></i>
             </button>
-          </li> 
-        `
-      );
-    }, "");
+          </li>
+    `
+    );
+  }, "");
 
-    progressUl.innerHTML = str;
-  } else {
-    progressUl.innerHTML = "";
-  }
+  let progressLis = state.progress.reduce((acc, curr) => {
+    return (
+      acc +
+      `
+      <li class="todo" data-id=${curr.id}>
+      <button class="btn previous">
+        <i class="fa-solid fa-angle-left"></i>
+      </button>
+      ${curr.name}
+      <button class="btn next">
+        <i class="fa-solid fa-angle-right"></i>
+      </button>
+    </li>
+    `
+    );
+  }, "");
+
+  let completeLis = state.complete.reduce((acc, curr) => {
+    return (
+      acc +
+      `
+      <li class="todo" data-id=${curr.id}>
+            <button class="btn previous">
+              <i class="fa-solid fa-angle-left"></i>
+            </button>
+            ${curr.name}
+          </li>
+    `
+    );
+  }, "");
+
+  taskUl.innerHTML = lis;
+  progressUl.innerHTML = progressLis;
+  completeUl.innerHTML = completeLis;
 }
 
-function showCompleteUI() {
-  if (state.complete.length) {
-    let str = state.complete.reduce((acc, curr) => {
-      return (
-        acc +
-        `
-        <li class="todo">
-        <button class="btn previous">
-          <i class="fa-solid fa-angle-left"></i>
-        </button>
-        ${curr}
-      </li>
-        `
-      );
-    }, "");
-
-    completeUl.innerHTML = str;
-  } else {
-    completeUl.innerHTML = "";
-  }
-}
-
-function addToProgress(e) {
+function taskClick(e) {
   if (e.target.classList.contains("fa-angle-right")) {
-    if (
-      !state.progress.includes(
-        e.target.parentElement.parentElement.textContent.trim()
-      )
-    ) {
-      state.progress.push(
-        e.target.parentElement.parentElement.textContent.trim()
-      );
-      e.target.parentElement.parentElement.remove();
-
-      showProgressUI();
-    } else {
-      state.complete.push(
-        e.target.parentElement.parentElement.textContent.trim()
-      );
-      e.target.parentElement.parentElement.remove();
-      showCompleteUI();
-    }
+    let elId = e.target.parentElement.parentElement.getAttribute("data-id");
+    let { foundElement, foundIdx } = findSpecificElement(elId);
+    state[roomNames[foundElement.room]].splice(foundIdx, 1);
+    moveTaskToSpecificRoom(foundElement, "right");
+  } else if (e.target.classList.contains("fa-angle-left")) {
+    let elId = e.target.parentElement.parentElement.getAttribute("data-id");
+    let { foundElement, foundIdx } = findSpecificElement(elId);
+    console.log(foundElement, foundIdx);
+    state[roomNames[foundElement.room]].splice(foundIdx, 1);
+    moveTaskToSpecificRoom(foundElement, "left");
   }
 }
 
-function addToComplete(e) {
-  if (e.target.classList.contains("fa-angle-right")) {
-    if (
-      !state.complete.includes(
-        e.target.parentElement.parentElement.textContent.trim()
-      )
-    ) {
-      state.complete.push(
-        e.target.parentElement.parentElement.textContent.trim()
-      );
-      state.progress.shift();
-      e.target.parentElement.parentElement.remove();
-      showCompleteUI();
-    }
+function findSpecificElement(id) {
+  let totalMergedArr = [...state.task, ...state.progress, ...state.complete];
+  let foundElement, foundIdx;
+  foundElement = totalMergedArr.find((element, idx) => {
+    foundIdx = idx;
+    return element.id === id;
+  });
+
+  return {
+    foundElement,
+    foundIdx,
+  };
+}
+
+function moveTaskToSpecificRoom(obj, direction) {
+  if (direction === "right") {
+    let modifiedObj = {
+      ...obj,
+      room: ++obj.room,
+      moveForward: obj.room !== 3 ? true : false,
+      moveBackward: obj.room !== 1 ? true : false,
+    };
+
+    let selectedRoom = roomNames[modifiedObj.room];
+    state[selectedRoom].push(modifiedObj);
+    console.log(state);
+    renderTask();
+  } else {
+    let modifiedObj = {
+      ...obj,
+      room: --obj.room,
+      moveForward: obj.room !== 3 ? true : false,
+      moveBackward: obj.room !== 1 ? true : false,
+    };
+
+    let selectedRoom = roomNames[modifiedObj.room];
+    state[selectedRoom].push(modifiedObj);
+    renderTask();
   }
 }
 
-// Add Event Listeners
-li.forEach((item) => item.addEventListener("click", addToProgress));
-progressUl.addEventListener("click", addToComplete);
+window.addEventListener("load", renderTask);
+taskUl.addEventListener("click", taskClick);
+progressUl.addEventListener("click", taskClick);
